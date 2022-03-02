763b8570f5344b681c1c03fbfbc15ddb5e7a9cd9
unknown <markovic.bosko2000@gmail.com>
2022-03-02 16:22:22 +0100

    Initial commit

---
+++ src/Initcode.jsx
@@ -0,0 +1,17 @@
+export const mdCode = `
+# Header 1
+- list item one
+- list item two
+[Free Code Camp](https://freecodecamp.org)
+### Text Decorations
+**bold**
+## Header 2
+![Cute Baby Panda](https://upload.wikimedia.org/wikipedia/commons/3/39/FreeCodeCamp_logo.png)
+> To be, or not be. That is a stupid question.
+\`npm install create-react-app -g\`
+\`\`\`
+function helloWorld() {
+  console.log("Hello World");
+}
+\`\`\`
+`;
